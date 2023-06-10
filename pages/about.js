d0d701ab368f4352e4f5b8918c9ceaffdecfa5b9
Milltiades <gigakakulia2017@gmail.com>
2023-06-10 19:01:22 +0400

    ninja-list frist nextjs

---
+++ pages/about.js
@@ -0,0 +1,31 @@
+import Head from "next/head";
+
+const About = () => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs. The passage is attributed to
+          an unknown typesetter in the 15th century who is thought to have
+          scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a
+          type specimen book. It usually begins with:
+        </p>
+        <p>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs. The passage is attributed to
+          an unknown typesetter in the 15th century who is thought to have
+          scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a
+          type specimen book. It usually begins with:
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs. The passage is attributed to
+          an unknown typesetter in the 15th century who is thought to have
+          scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a
+          type specimen book. It usually begins with:
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs. The passage is attributed to
+          an unknown typesetter in the 15th century who is thought to have
+          scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a
+          type specimen book. It usually begins with:
+        </p>
+        <Link href="/ninjas" className={styles.btn}>
+          See Ninja Listing
+        </Link>
+      </div>
+    </>
+  );
+}
